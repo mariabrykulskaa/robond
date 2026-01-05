fa7f92dbb49852770dc55452c1969f5b9aff6f93
Alexander Shirokovskikh <alex.shir.141@gmail.com>
2026-01-05 17:10:28 +0300

    Add add function

--- project1/src/main.rs
+++ project1/src/main.rs
@@ -3,3 +3,29 @@
 fn main() {
     println!("Project 1!");
 }
+
+fn add(a: i32, b: i32) -> i32 {
+    let mut sum = 0;
+    for _i in 0..a {
+        sum += 1;
+    }
+    for _i in 0..b {
+        sum += 1;
+    }
+    sum
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_simple() {
+        assert_eq!(add(2, 2), 4);
+    }
+
+    #[test]
+    fn test_add_with_negative_number() {
+        assert_eq!(add(5, -2), 3);
+    }
+}
