667ae5b5c8ebccdf1947107c633b3d3183409feb
Alexander Shirokovskikh <alex.shir.141@gmail.com>
2026-03-15 00:33:24 +0300

    Refactor code

--- t-invest-api-rust/src/error.rs
+++ t-invest-api-rust/src/error.rs
@@ -1,12 +1,16 @@
 use thiserror::Error;
 
+/// Алиас `Result`, в котором вариант `Err` — это `t_invest_api_rust::Error`.
 pub type Result<T> = std::result::Result<T, Error>;
 
+/// Ошибки при создании клиента
 #[derive(Error, Debug)]
 pub enum Error {
+    /// Ошибка транспорта
     #[error("transport error")]
     Transport(#[from] tonic::transport::Error),
 
+    /// Токен авторизации содержит недопустимые символы
     #[error("authorization token contains invalid characters")]
     InvalidAuthorizationTokenCharacters(#[from] tonic::metadata::errors::InvalidMetadataValue),
 }
