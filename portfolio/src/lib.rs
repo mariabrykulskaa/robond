9bc01f11180f09b6c6613b8fabda6ef1a04abf88
Maria Brykulskaya <mabrykulskaia@edu.hse.ru>
2026-03-23 17:53:28 +0300

    fix: replace rust_decimal_macros with Decimal::from_str in doc example

--- portfolio/src/lib.rs
+++ portfolio/src/lib.rs
@@ -13,13 +13,15 @@
 //!
 //! ```no_run
 //! use portfolio::PortfolioClient;
+//! use rust_decimal::Decimal;
+//! use std::str::FromStr;
 //!
 //! # async fn example() -> portfolio::Result<()> {
 //! let client = PortfolioClient::from_env().await?;
 //! client.run_migrations().await?;
 //!
 //! let p = client.create_portfolio("Мой портфель").await?;
-//! client.set_cash(p.id, rust_decimal_macros::dec!(1_000_000), "RUB").await?;
+//! client.set_cash(p.id, Decimal::from_str("1000000").unwrap(), "RUB").await?;
 //! client.set_holding(p.id, "RU000A0ZZBC7", 100).await?;
 //! # Ok(())
 //! # }
